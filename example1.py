5ed23f606a031ba70a3dbdd2ba785b46a8c3ad5e
aamalik7196 <aamalik7196@gmail.com>
2020-06-24 15:09:54 -0400

    sphinx demo

---
+++ example1.py
@@ -0,0 +1,15 @@
+# Author: Fluence
+
+
+# Addition
+def add(x=0, y=0):
+    """
+    add function returns the sum of two numbers
+    :param x: first number
+    :param y: second number
+    :return: sum of first and second
+    """
+    return x + y
+
+
+print(add(5, 7))
